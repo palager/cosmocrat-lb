a6226413b11ff1d95c742d82be0248b4755bb50c
Patrick Gallagher <patrick@cosmocr.at>
2019-08-31 14:15:58 -0400

    correct https port

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -7,13 +7,13 @@ func main() {
 	var p tcpproxy.Proxy
 
 	p.AddHTTPHostRoute(":80", "upspin.cosmocr.at", tcpproxy.To("upspin-backend.default.svc.cluster.local:80"))
-	p.AddSNIRoute(":443", "upspin.cosmocr.at", tcpproxy.To("upspin-backend.default.svc.cluster.local:8443"))
+	p.AddSNIRoute(":443", "upspin.cosmocr.at", tcpproxy.To("upspin-backend.default.svc.cluster.local:443"))
 
 	p.AddHTTPHostRoute(":80", "135ppw.nyc", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
-	p.AddSNIRoute(":443", "135ppw.nyc", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:8443"))
+	p.AddSNIRoute(":443", "135ppw.nyc", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
 
 	p.AddHTTPHostRoute(":80", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
-	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:8443"))
+	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
 
 	p.AddRoute(":8008", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8008"))
 	log.Fatal(p.Run())
