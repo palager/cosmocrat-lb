aab68dfa5859982ae8301691d886face571bed8b
Patrick Gallagher <patrick@cosmocr.at>
2019-08-31 14:20:44 -0400

    add cosmocr.at

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -16,5 +16,8 @@ func main() {
 	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
 
 	p.AddRoute(":8008", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8008"))
+
+	p.AddHTTPHostRoute(":80", "cosmocr.at", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
+	p.AddSNIRoute(":443", "cosmocr.at", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
 	log.Fatal(p.Run())
 }
