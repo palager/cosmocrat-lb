78287fbf9c3be8fc5f97c09df69f5bb0995ca0b1
Patrick Gallagher <patrick@cosmocr.at>
2020-01-21 20:02:09 -0500

    fix ports for ssb-viewer

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -16,8 +16,8 @@ func main() {
 	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
 
 	p.AddRoute(":8008", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8008"))
-	p.AddHTTPHostRoute(":80", "ssb.cosmocr.at", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8080"))
-	p.AddSNIRoute(":443", "ssb.cosmocr.at", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8443"))
+	p.AddHTTPHostRoute(":80", "ssb.cosmocr.at", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:80"))
+	p.AddSNIRoute(":443", "ssb.cosmocr.at", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:443"))
 
 	p.AddHTTPHostRoute(":80", "cosmocr.at", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
 	p.AddSNIRoute(":443", "cosmocr.at", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
