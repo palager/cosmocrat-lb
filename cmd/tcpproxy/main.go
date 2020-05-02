dd37d0472e00eb215a8efbe0649b70bc7daa31d3
Patrick Gallagher <rpg@google.com>
2020-05-01 22:30:04 -0400

    move everything to homebase

--- cmd/tcpproxy/main.go
+++ cmd/tcpproxy/main.go
@@ -9,11 +9,11 @@ func main() {
 	p.AddHTTPHostRoute(":80", "upspin.cosmocr.at", tcpproxy.To("upspin-backend.default.svc.cluster.local:80"))
 	p.AddSNIRoute(":443", "upspin.cosmocr.at", tcpproxy.To("upspin-backend.default.svc.cluster.local:443"))
 
-	p.AddHTTPHostRoute(":80", "135ppw.nyc", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
-	p.AddSNIRoute(":443", "135ppw.nyc", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
+	p.AddHTTPHostRoute(":80", "135ppw.nyc", tcpproxy.To("homebase-service.default.svc.cluster.local:80"))
+	p.AddSNIRoute(":443", "135ppw.nyc", tcpproxy.To("homebase-service.default.svc.cluster.local:443"))
 
-	p.AddHTTPHostRoute(":80", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:80"))
-	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("cosmocrat-backend.default.svc.cluster.local:443"))
+	p.AddHTTPHostRoute(":80", "135ppw.org", tcpproxy.To("homebase-service.default.svc.cluster.local:80"))
+	p.AddSNIRoute(":443", "135ppw.org", tcpproxy.To("homebase-service.default.svc.cluster.local:443"))
 
 	p.AddRoute(":8008", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:8008"))
 	p.AddHTTPHostRoute(":80", "ssb.cosmocr.at", tcpproxy.To("ssb-pub.scuttlebutt.svc.cluster.local:80"))
